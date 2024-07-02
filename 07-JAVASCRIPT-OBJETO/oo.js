764e686f98c1198e1492bee4aeff9e0f0234fb46
danielsilvalima1996 <dsl15021996@gmail.com>
2024-07-02 20:51:57 -0300

    finalizado modulo 07 objeto

---
+++ 07-JAVASCRIPT-OBJETO/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() //foco pasou a ser no objeto
+
+// Princípios importantes:
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. Polimorfismo
